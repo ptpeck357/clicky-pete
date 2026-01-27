4d8b71a968f6b3f2cab3f01f95c9724df3faa6a0
Peter Peck <petertpeck@gmail.com>
2026-01-27 13:29:53 -0700

    feat: new icons

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -44,15 +44,20 @@ export const Header: React.FC = () => {
 			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 				<div className="flex justify-between items-center h-16">
 					<Link to="/" className="flex items-center space-x-2" onClick={() => setIsMobileMenuOpen(false)}>
-						<div className="w-8 h-8 bg-blue-600 rounded-lg flex items-center justify-center">
-							<svg className="w-5 h-5 text-white" fill="currentColor" viewBox="0 0 20 20">
-								<path
-									fillRule="evenodd"
-									d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z"
-									clipRule="evenodd"
-								/>
+						<div className="w-8 h-8 bg-neutral-900 rounded-lg flex items-center justify-center">
+							<svg
+								className="w-5 h-5 text-white"
+								viewBox="0 0 24 24"
+								fill="none"
+								stroke="currentColor"
+								strokeWidth="2"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+							>
+								<path d="M3 18l6-8 4 5 3-4 5 7" />
 							</svg>
 						</div>
+
 						<span className="text-xl font-bold text-white">Clicky Pete</span>
 					</Link>
 
