360cf21975818d9aa0f2da3e9ae1fb7cc757ede9
Peter Peck <petertpeck@gmail.com>
2026-01-29 00:57:09 -0700

    fix: darken background when modal is open

--- src/components/organisms/PhotoViewerModal/PhotoViewerModal.tsx
+++ src/components/organisms/PhotoViewerModal/PhotoViewerModal.tsx
@@ -133,7 +133,7 @@ export const PhotoViewerModal: React.FC<PhotoViewerModalProps> = ({ photo, isOpe
 					transition={{ duration: 0.3 }}
 				>
 					<motion.div
-						className="absolute inset-0 bg-black/40 backdrop-blur-md"
+						className="absolute inset-0 bg-black/60 backdrop-blur-md"
 						initial={{ opacity: 0 }}
 						animate={{ opacity: 1 }}
 						exit={{ opacity: 0 }}
