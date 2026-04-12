1e1724d4cfc812810a63a6b2bb7b6e10ae0a9a02
Peter Peck <petertpeck@gmail.com>
2026-04-12 13:56:28 -0600

    feat: gate journal routes behind VITE_ENVIRONMENT flag

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import { Gallery } from '../pages/Gallery';
 import { About } from '../pages/About';
@@ -7,6 +7,8 @@ import { Contact } from '../pages/Contact';
 import { Journal } from '../pages/Journal';
 import { JournalDetail } from '../pages/JournalDetail';
 import { NotFound } from '../pages/NotFound';
+
+const isDev = import.meta.env.VITE_ENVIRONMENT === 'dev';
 
 export const Router: React.FC = () => {
 	return (
@@ -16,8 +18,8 @@ export const Router: React.FC = () => {
 			<Route path="/gallery/:collection" element={<Gallery />} />
 			<Route path="/about" element={<About />} />
 			<Route path="/contact" element={<Contact />} />
-			<Route path="/journal" element={<Journal />} />
-			<Route path="/journal/:id" element={<JournalDetail />} />
+			<Route path="/journal" element={isDev ? <Journal /> : <Navigate to="/404" replace />} />
+			<Route path="/journal/:id" element={isDev ? <JournalDetail /> : <Navigate to="/404" replace />} />
 			<Route path="*" element={<NotFound />} />
 		</Routes>
 	);
